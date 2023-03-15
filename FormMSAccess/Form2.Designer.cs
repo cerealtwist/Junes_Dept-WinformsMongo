2fc4995205115c1116ad5f1722c54b33fb4aae03
0Diy <ventriloquist225@gmail.com>
2023-03-15 14:53:49 +0700

    Membuat Form Login: Form2

---
+++ FormMSAccess/Form2.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace FormMSAccess
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.titleBox = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new FormMSAccess.Customs.CustomButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.user_label = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.titleBox)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.titleBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(541, 562);
+            this.panel1.TabIndex = 0;
+            // 
+            // titleBox
+            // 
+            this.titleBox.Image = ((System.Drawing.Image)(resources.GetObject("titleBox.Image")));
+            this.titleBox.Location = new System.Drawing.Point(70, 180);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(418, 200);
+            this.titleBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.titleBox.TabIndex = 1;
+            this.titleBox.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblError);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnLogin);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtPassword);
+            this.panel2.Controls.Add(this.user_label);
+            this.panel2.Controls.Add(this.txtUsername);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(539, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(404, 562);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(43, 457);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(15, 20);
+            this.lblError.TabIndex = 24;
+            this.lblError.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Helvetica", 18.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(43, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(289, 37);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Login to Inventory";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(43, 379);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(300, 38);
+            this.btnLogin.TabIndex = 22;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Helvetica", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(58, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Location = new System.Drawing.Point(43, 321);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(300, 27);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // user_label
+            // 
+            this.user_label.AutoSize = true;
+            this.user_label.Font = new System.Drawing.Font("Helvetica", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.user_label.Location = new System.Drawing.Point(58, 252);
+            this.user_label.Name = "user_label";
+            this.user_label.Size = new System.Drawing.Size(73, 16);
+            this.user_label.TabIndex = 1;
+            this.user_label.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.SystemColors.Control;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Location = new System.Drawing.Point(43, 260);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(300, 27);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txt_Username_TextChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 562);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.titleBox)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label user_label;
+        private System.Windows.Forms.TextBox txtUsername;
+        private Customs.CustomButton btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox titleBox;
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ FormMSAccess/Form2.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormMSAccess
+{
+    public partial class Form2 : Form
+    {
+
+        public static IMongoClient client = new MongoClient();
+
+        public static IMongoDatabase db = client.GetDatabase("inventaris_db");
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // query Database MongoDB untuk mencari user dengan credentials yang valid.
+            var usersCollection = db.GetCollection<BsonDocument>("users");
+            var filter = Builders<BsonDocument>.Filter.And(
+                Builders<BsonDocument>.Filter.Eq("username", username),
+                Builders<BsonDocument>.Filter.Eq("password", password)
+            );
+            var result = usersCollection.Find(filter).FirstOrDefault();
+
+            if (result != null)
+            {
+                // Jika Autentikasi Berhasil
+                Form1 mainForm = new Form1();
+                mainForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                // Jika Autentikasi Gagal
+                lblError.Text = "*Invalid Credentials.";
+            }
+        }
+
+        private void txt_Username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
